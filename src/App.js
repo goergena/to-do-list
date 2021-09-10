c16f700a4ecfab615d6c256aaf89bf92dfa09bc6
Ali Goergen <alyssa.goergen@gmail.com>
2021-09-09 22:03:40 -0400

    project outline

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        test text
-      </header>
+      <div className="App-body">
+        <header>
+          <h1>To Do List</h1>
+        </header>
+        <button>Give me something to do</button>
+        <ol>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ol>
+        <form>
+          <input type='text'/>
+          <button>Add Task</button>
+        </form>
+      </div>
     </div>
   );
 }
