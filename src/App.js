7c6855fcb0798f1c66d95ed321a55efa892d334e
Ali Goergen <alyssa.goergen@gmail.com>
2021-09-10 20:57:14 -0400

    stateful tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-body">
-        <header>
-          <h1>To Do List</h1>
-        </header>
-        <button>Give me something to do</button>
-        <ol>
-          <li>item 1</li>
-          <li>item 2</li>
-          <li>item 3</li>
-        </ol>
-        <form>
-          <input type='text'/>
-          <button>Add Task</button>
-        </form>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: ['add', 'commit', 'push']
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-body">
+          <header>
+            <h1>To Do List</h1>
+          </header>
+          <button>Give me something to do</button>
+          {/* <ol>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ol> */}
+          <TaskList tasks={this.state.tasks}/>
+          <form>
+            <input type='text'/>
+            <button>Add Task</button>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+class TaskList extends React.Component {
+  render() {
+    return (
+      <ol>
+      {
+          this.props.tasks.map((task, i) => <Task key={i} taskName={task}/>)
+        }
+    </ol>
+    )
+  }
+}
+
+class Task extends React.Component {
+  render() {
+    return (
+      <li>
+        {this.props.taskName}
+      </li>
+    )
+  }
+}
+
+
+
+
 
 export default App;
