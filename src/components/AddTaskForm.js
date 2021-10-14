8b63ae873991e24a9e4945b26e495a7b7f5105e4
Ali Goergen <alyssa.goergen@gmail.com>
2021-10-13 22:20:27 -0400

    form style

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -4,18 +4,30 @@ class AddTaskForm extends React.Component {
     constructor(props) {
       super(props)
       this.addNewTask = this.addNewTask.bind(this)
+      this.state = {
+        error: undefined
+      }
     }
+
     addNewTask(e) {
       e.preventDefault();
       let newTask = e.target.elements.newtask.value.trim();
-      this.props.addNewTask(newTask)
+      const error = this.props.addNewTask(newTask)
+      this.setState(()=> ({ error }))
+      if (!error) {
+        e.target.elements.option.value = '';
+      }
     }
     render() {
       return (
-        <form onSubmit={this.addNewTask}>
-        <input type='text' name='newtask'/>
-        <input type='submit' value='Submit' className='submit-btn button'/>
-      </form>
+        <div>
+          {this.state.error && <p className="add-task-error">{this.state.error}</p>}
+          <form className="add-task" onSubmit={this.addNewTask}>
+            <input className="add-task__input" type='text' name='newtask'/>
+            <input type='submit' value='Submit' className='submit-btn button'/>
+          </form>
+        </div>
+
       )
     }
   }
